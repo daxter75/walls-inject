9995fb822f7290d43e2d6e35b786b5633e43da81
daxter75 <daxter75@gmail.com>
2021-10-20 16:13:51 +0200

    getInjectedMethod

--- src/Inject.php
+++ src/Inject.php
@@ -17,6 +17,20 @@ class Inject
 
     public function getInjected(): array
     {
-        return [];
+        $insert[$this->newKey] = $this->newValue;
+        $arrayKeys = array_keys($this->initArray);
+
+        if (array_search($this->newKey, $arrayKeys, true) !== false) {
+            unset($this->initArray[$this->newKey]);
+            $arrayKeys = array_keys($this->initArray);
+        }
+
+        $index = array_search($this->afterKey, $arrayKeys, true);
+        $pos = $index === false ? count($this->initArray) : $index + 1;
+
+        return
+            array_slice($this->initArray, 0, $pos, true) +
+            $insert +
+            array_slice($this->initArray, $pos, count($this->initArray) - 1);
     }
 }
